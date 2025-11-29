750beff78660cc9f8da627c8415bf100da6242ef
Jakub <bubliksiur@gmail.com>
2025-11-29 22:04:59 +0100

    added some pages

---
+++ portfolio/app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import Hello from "../components/hello";
+
+export default function Home() {
+  return (
+    <>
+      <div className="test-3xl">Hello, I would like to learn this framework</div>
+      <Hello />
+    </>
+  );
+}
--- portfolio/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div>Hello, I would like to learn this framework</div>
-  );
-}
